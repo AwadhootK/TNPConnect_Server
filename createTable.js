e8790f05c5a2b5b2b17811f5b9b503d898b0b5e4
Awadhoot <awadhootk6@gmail.com>
2024-03-29 14:34:31 +0530

    dynamic table creation successful

--- createTable.js
+++ createTable.js
@@ -1,40 +1,46 @@
-// const { Pool } = require('pg')
+const { Pool } = require('pg')
+require('dotenv').config()
 
 // Create a PostgreSQL connection pool
-// const pool = new Pool({
-//   connectionString: 'your_database_connection_string'
-// })
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+})
 
 async function createTable(
   tableName,
   columnNames,
   columnTypes
 ) {
-  // const client = await pool.connect()
+  const client = await pool.connect()
   try {
 
     const query = `CREATE TABLE IF NOT EXISTS "${tableName}" (
-      enrollmentNo PRIMARY KEY,
+      enrollmentNo VARCHAR(255) PRIMARY KEY,
       ${columnNames
         .map((columnName, index) => `"${columnName}" ${columnTypes[index]}`)
-      .join(',\n')},
-        FOREIGN KEY (enrollmentNo) REFERENCES Student(enrollmentNo) 
+        .join(',\n')},
+        FOREIGN KEY (enrollmentNo) REFERENCES "Student"("enrollmentNo") 
         ON DELETE CASCADE 
         ON UPDATE CASCADE
-    )`
+    )`;
 
     console.log(query)
+
     // Execute the query
-    // await client.query(query)
-    // console.log(`Table "${tableName}" created successfully.`)
+    await client.query(query)
+
+    console.log(`Table "${tableName}" created successfully.`)
+
   } catch (error) {
     console.error('Error creating table:', error)
   } finally {
     // Release the client back to the pool
-    // client.release()
+    client.release();
   }
 }
 
+
+// get the columns and the types from admin web frontend 
 createTable('Barclays', ['name', 'age'], ['VARCHAR(255)', 'INTEGER'])
   .then(() => {
     // Do something after the table is created
